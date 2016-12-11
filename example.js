9e672b44dc118c134a722b8c7332367f6d93b62f
cy59n05y5 <juny1905@naver.com>
2016-12-11 19:55:54 +0900

    example.js mod

--- example.js
+++ example.js
@@ -2,12 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('<TITLE>Example</TITLE>');
-    res.send('Hello World!');
-    res.send('')
-});
-
+app.use(express.static(__dirname, '/public'));
 app.listen(3000, function() {
     console.log('Server On!');
 })
