b3ca554e5933b97837825ceb46c1265a40631fc9
cy59n05y5 <juny1905@naver.com>
2016-12-11 22:32:51 +0900

    Add EJS

--- public/script/script.js
+++ public/script/script.js
@@ -7,3 +7,10 @@ console.log("원래 문장 : " + name);
 document.getElementsByTagName('h1')[0].firstChild.nodeValue = '문서를 조작했다 !';
 
 */
+
+nodeCount = document.getElementsByTagName('li').length;
+for (i = 0; i < nodeCount; i++) {
+    document.getElementsByTagName('li')[i].firstChild.nodeValue = (i + 1) + "번째 노드";
+}
+
+console.log(nodeCount + "개의 텍스트 수정하였음.");
