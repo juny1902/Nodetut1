a483c4b21c70bbed9eb560f1f58a93b8f4c779ef
cy59n05y5 <juny1905@naver.com>
2016-12-11 22:27:08 +0900

    계속 연습하고 있어요

---
+++ public/script/script.js
@@ -0,0 +1,9 @@
+/*
+
+var name = document.getElementsByTagName('h1')[0].firstChild.nodeValue;
+
+console.log("원래 문장 : " + name);
+
+document.getElementsByTagName('h1')[0].firstChild.nodeValue = '문서를 조작했다 !';
+
+*/
