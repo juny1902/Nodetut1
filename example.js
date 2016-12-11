d282aee25c50af5dbc3d5d5035ea1978987c790f
cy59n05y5 <juny1905@naver.com>
2016-12-11 19:50:58 +0900

    Hi

--- example.js
+++ example.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
+    res.send('<TITLE>Example</TITLE>');
     res.send('Hello World!');
+    res.send('')
 });
 
 app.listen(3000, function() {
