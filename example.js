493d6b390b0750479ae559c7c6008fe8bc5acc25
cy59n05y5 <juny1905@naver.com>
2016-12-11 20:10:25 +0900

    port 3000 -> 4000

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(path.join(__dirname + 'public')));
 
-app.listen(3000, function() {
+app.listen(4000, function() {
     console.log('Server On!');
 });
